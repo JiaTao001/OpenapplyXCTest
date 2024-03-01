6d8f6a59a15619ea641bda9f9ec72846dbb53fcb
Faria01 <2233945666@qq.com>
2024-03-01 14:47:29 +0800

    init

---
+++ OAXCTest/OAXCTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OAXCTest
+//
+//  Created by Tao.jia on 2024/3/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OAXCTest/OAXCTest/OAXCTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OAXCTestApp.swift
+//  OAXCTest
+//
+//  Created by Tao.jia on 2024/3/1.
+//
+
+import SwiftUI
+
+@main
+struct OAXCTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
